f7ea8164f461eb9f028c3056f21627f0552483ad
Koen Zandberg <koen@bergzand.net>
2020-01-18 15:05:14 +0100

    tests/cst816s: Add basic touch screen test

--- tests/driver_cst816s/main.c
+++ tests/driver_cst816s/main.c
@@ -13,7 +13,7 @@
  * @file
  * @brief       Test application for the CST816S touch screen driver
  *
- * @author      Koen Zandberg <koen@bergzand.net>
+ * @author      koen Zandberg <koen@bergzand.net>
  *
  * @}
  */
@@ -23,44 +23,57 @@
 
 #include "cst816s_params.h"
 #include "cst816s.h"
+#include "xtimer.h"
+#include "fmt.h"
 #include "thread.h"
 #include "thread_flags.h"
+
+static kernel_pid_t _pid;
 
 #define CST816S_THREAD_FLAG     (1 << 8)
 #define CST816S_NUM_TOUCHES     5
 
-static void _cb(void *arg)
+static void _cb(cst816s_t *dev, void *arg)
 {
-    kernel_pid_t *pid = arg;
-    thread_flags_set((thread_t *)sched_threads[*pid], CST816S_THREAD_FLAG);
+    (void)dev;
+    (void)arg;
+    thread_flags_set((thread_t*)sched_threads[_pid], CST816S_THREAD_FLAG);
 }
 
-static void _dump_cst816s(cst816s_t *dev)
+static int _dump_cst816s(cst816s_t *dev)
 {
     puts("Reading data:");
-    cst816s_touch_data_t touches;
-    if (cst816s_read(dev, &touches) == 0) {
-        printf("Touch at %03u, %03u with gesture type \"%s\"\n", touches.x,
-               touches.y, cst816s_gesture_str[touches.gesture]);
+    cst816s_touch_data_t touches[CST816S_NUM_TOUCHES];
+    int num = cst816s_read(dev, touches, CST816S_NUM_TOUCHES);
+    if (num > 0) {
+        for (size_t i = 0; i < (size_t)num; i++) {
+            printf("Touch %u at %03u, %03u with finger %u\n", i, touches[i].x,
+                   touches[i].y, touches[i].finger);
+        }
+    }
+    else if (num == 0) {
+        puts("No touch detected on the screen");
     }
     else {
         puts("Device not responding");
     }
+    return num;
 }
 
 int main(void)
 {
     cst816s_t dev;
-
-    kernel_pid_t pid = thread_getpid();
+    _pid = sched_active_pid;
 
     puts("CST816S test application\n");
-    cst816s_init(&dev, &cst816s_params[0], _cb, &pid);
+    cst816s_init(&dev, &cst816s_params[0], _cb, NULL);
 
     while (1) {
         thread_flags_t flags = thread_flags_wait_any(CST816S_THREAD_FLAG);
         if (flags & CST816S_THREAD_FLAG) {
-            _dump_cst816s(&dev);
+            while (_dump_cst816s(&dev) >= 0) {
+                xtimer_usleep(500 * US_PER_MS);
+            }
         }
     }
     return 0;
